ee90b13d93b6c4798678eebf7422cc13110b042c
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-10-06 16:21:20 +0600

    Cookie setting updated

--- utils/index.js
+++ utils/index.js
@@ -14,9 +14,9 @@ function setCookie(res, user) {
   res.cookie(user.role.toLowerCase(), jwtToken, {
     httpOnly: true,
     path: "/",
-    secure: false,
-    sameSite: "none",
-    domain: process.env.SITE_URL,
+    // secure: false,
+    // sameSite: "none",
+    // domain: process.env.SITE_URL,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
   });
 }
