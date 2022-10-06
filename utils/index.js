647652f006ecff3d870e8cf90207a3cd81afac59
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-10-06 13:08:49 +0600

    Cookie bug fixed

--- utils/index.js
+++ utils/index.js
@@ -3,23 +3,19 @@ const jwt = require("jsonwebtoken");
 // Generate token and set cookie to header
 function setCookie(res, user) {
   // // Destructure user object
-  // const { _id, role } = user;
-
-  // console.log(id);
+  const { id, role } = user;
 
   // Generate token
-  const jwtToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+  const jwtToken = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   // Set cookie to header
-  res.cookie(user.role.toLowerCase(), jwtToken, {
+  res.cookie(role.toLowerCase(), jwtToken, {
     httpOnly: true,
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-    // sameSite: "strict",
     secure: process.env.NODE_ENV !== "development",
-    // domain: process.env.SITE_URL,
   });
 }
 
