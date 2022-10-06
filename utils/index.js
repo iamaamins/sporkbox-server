664a62d296ea608b6edf71c9f842e2e8bcb8d00a
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-10-06 16:13:00 +0600

    Cookie setting updated

--- utils/index.js
+++ utils/index.js
@@ -6,12 +6,12 @@ function setCookie(res, user) {
   const { id, role } = user;
 
   // Generate token
-  const jwtToken = jwt.sign({ id }, process.env.JWT_SECRET, {
+  const jwtToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   // Set cookie to header
-  res.cookie(role.toLowerCase(), jwtToken, {
+  res.cookie(user.role.toLowerCase(), jwtToken, {
     httpOnly: true,
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
