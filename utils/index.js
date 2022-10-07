2fda5c53c1d4c66d78bece1c59dbc6e62d340957
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-10-07 07:15:11 +0600

    Testing cookie

--- utils/index.js
+++ utils/index.js
@@ -15,7 +15,7 @@ function setCookie(res, user) {
     httpOnly: true,
     path: "/",
     domain: "https://vercel.app/",
-    // sameSite: "none",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
     secure: process.env.NODE_ENV !== "development",
   });
