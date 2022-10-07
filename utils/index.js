cad0a27980aa105d8a17e411e7b3123f3b4f4b45
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-10-07 07:47:41 +0600

    Secure set to false

--- utils/index.js
+++ utils/index.js
@@ -16,7 +16,7 @@ function setCookie(res, user) {
     path: "/",
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-    secure: process.env.NODE_ENV !== "development",
+    secure: false,
   });
 }
 
