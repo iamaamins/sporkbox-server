554620118573b8559641fa83607985f7774390b1
Alamin Shaikh <hello@octib.com>
2022-12-06 07:57:47 +0600

    Origin URL changed to localhost

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: "https://www.araqo.com",
+    origin: "http://localhost:3000",
   })
 );
 
