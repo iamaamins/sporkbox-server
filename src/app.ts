cfa726c5edc57273888445195b9cd8f05f219ff6
Alamin Shaikh <hello@octib.com>
2022-12-14 15:35:41 +0600

    Origin URL updated

--- src/app.ts
+++ src/app.ts
@@ -32,13 +32,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: "https://www.sporkbox.octib.com",
+    origin: "https://sporkbox.octib.com",
   })
 );
 
 // http://localhost:3000
 // https://www.sporkbox.app
-// https://www.sporkbox.octib.com
+// https://sporkbox.octib.com
 
 // Routes
 app.use("/users", User);
