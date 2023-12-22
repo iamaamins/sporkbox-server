e18eb4946364f13a958f1699cf9850b0fe6a710d
Alamin Shaikh <hello@octib.com>
2023-12-22 15:55:46 +0600

    Item stat route created

--- src/routes/stat.ts
+++ src/routes/stat.ts
@@ -3,15 +3,26 @@ import Order from '../models/order';
 
 const router = Router();
 
-type Order = {
-  restaurant: {
-    id: string;
-    name: string;
-  };
+type Restaurant = {
+  id: string;
+  name: string;
+};
+
+type OrderData = {
+  restaurant: Restaurant;
   quantity: number;
 };
 
-router.get('/total-orders', async (req, res) => {
+type ItemData = {
+  restaurant: Restaurant;
+  item: {
+    id: string;
+    name: string;
+    quantity: number;
+  };
+};
+
+router.get('/order', async (req, res) => {
   // Get all delivered orders
   const orders = await Order.find({ status: 'DELIVERED' }).lean().orFail();
 
@@ -41,9 +52,60 @@ router.get('/total-orders', async (req, res) => {
         }
       });
     }
-  }, [] as Order[]);
+  }, [] as OrderData[]);
 
   // Return the response
+  res.status(200).json(results);
+});
+
+router.get('/item', async (req, res) => {
+  // Get all delivered orders
+  const orders = await Order.find({ status: 'DELIVERED' }).lean().orFail();
+
+  // Create results
+  const results = orders.reduce((acc, curr) => {
+    if (
+      !acc.some(
+        (order) =>
+          order.item.id === curr.item._id.toString() &&
+          order.restaurant.id === curr.restaurant._id.toString()
+      )
+    ) {
+      return [
+        ...acc,
+        {
+          restaurant: {
+            id: curr.restaurant._id.toString(),
+            name: curr.restaurant.name,
+          },
+          item: {
+            id: curr.item._id.toString(),
+            name: curr.item.name,
+            quantity: curr.item.quantity,
+          },
+        },
+      ];
+    } else {
+      return acc.map((order) => {
+        if (
+          order.item.id === curr.item._id.toString() &&
+          order.restaurant.id === curr.restaurant._id.toString()
+        ) {
+          return {
+            ...order,
+            item: {
+              ...order.item,
+              quantity: order.item.quantity + curr.item.quantity,
+            },
+          };
+        } else {
+          return order;
+        }
+      });
+    }
+  }, [] as ItemData[]);
+
+  // Return results
   res.status(200).json(results);
 });
 
