f5e5df5c39da41a439ffff9a63cb90e47dc05a40
Alamin Shaikh <hello@octib.com>
2024-01-11 12:29:38 +0600

    People stat added

--- src/routes/stat.ts
+++ src/routes/stat.ts
@@ -2,10 +2,31 @@ import { Router } from 'express';
 import Order from '../models/order';
 import { ItemStat, OrderStat } from '../types';
 import authUser from '../middleware/authUser';
+import { dateToMS } from '../utils';
 
 const router = Router();
 
-const companies = ['643dec49e88d25d4249723ef', '643e162fe88d25d424972a55'];
+async function getDeliveredOrders() {
+  try {
+    const orders = await Order.find({
+      status: 'DELIVERED',
+      'company._id': {
+        $in: ['643dec49e88d25d4249723ef', '643e162fe88d25d424972a55'],
+      },
+      'delivery.date': {
+        $gte: '2023-01-01',
+        $lte: '2023-12-31',
+      },
+    })
+      .lean()
+      .orFail();
+
+    return orders;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
 
 router.get('/order', authUser, async (req, res) => {
   if (req.user) {
@@ -14,12 +35,7 @@ router.get('/order', authUser, async (req, res) => {
 
     if (role === 'ADMIN') {
       // Get all delivered orders
-      const orders = await Order.find({
-        status: 'DELIVERED',
-        'company._id': { $in: companies },
-      })
-        .lean()
-        .orFail();
+      const orders = await getDeliveredOrders();
 
       // Create results
       const results = orders.reduce((acc, curr) => {
@@ -71,12 +87,7 @@ router.get('/item', authUser, async (req, res) => {
 
     if (role === 'ADMIN') {
       // Get all delivered orders
-      const orders = await Order.find({
-        status: 'DELIVERED',
-        'company._id': { $in: companies },
-      })
-        .lean()
-        .orFail();
+      const orders = await getDeliveredOrders();
 
       // Create results
       const results = orders.reduce((acc, curr) => {
@@ -130,6 +141,52 @@ router.get('/item', authUser, async (req, res) => {
       res.status(403);
       throw new Error('Not authorized');
     }
+  }
+});
+
+router.get('/people', authUser, async (req, res) => {
+  if (req.user) {
+    const { role } = req.user;
+
+    if (role === 'ADMIN') {
+      const orders = await getDeliveredOrders();
+
+      // Number of unique customers served on each day
+      const results = orders.reduce((acc, curr) => {
+        const currCustomerId = curr.customer._id.toString();
+        const currDeliveryDate = dateToMS(curr.delivery.date);
+
+        if (!acc.some((el) => el.date === currDeliveryDate)) {
+          return [
+            ...acc,
+            { date: currDeliveryDate, customers: [currCustomerId] },
+          ];
+        } else {
+          return acc.map((el) => {
+            if (el.date === currDeliveryDate) {
+              return {
+                ...el,
+                customers: el.customers.includes(currCustomerId)
+                  ? el.customers
+                  : [...el.customers, currCustomerId],
+              };
+            } else {
+              return el;
+            }
+          });
+        }
+      }, [] as { date: number; customers: string[] }[]);
+
+      res.status(200).json(results);
+    } else {
+      console.log('Not authorized');
+      res.status(403);
+      throw new Error('Not authorized');
+    }
+  } else {
+    console.log('Not authorized');
+    res.status(403);
+    throw new Error('Not authorized');
   }
 });
 
