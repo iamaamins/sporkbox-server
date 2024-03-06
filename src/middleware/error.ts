8e9953996be9c5cc5a57ce7f88a8f14fece1f1e2
Alamin Shaikh <hello@octib.com>
2024-03-06 10:46:22 +0600

    Error middleware refactored

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,10 +1,10 @@
 import { MulterError } from 'multer';
 import { ErrorRequestHandler } from 'express';
+import { invalidCredentials } from '../lib/messages';
 
 const handler: ErrorRequestHandler = (err, req, res, next) => {
-  // If err is a multer error
+  // Multer error
   if (err instanceof MulterError) {
-    // If unexpected file format is provided
     if (err.code === 'LIMIT_UNEXPECTED_FILE') {
       return res
         .status(400)
@@ -12,97 +12,72 @@ const handler: ErrorRequestHandler = (err, req, res, next) => {
     }
   }
 
-  // If error is a populate error
+  // Populate error
   if (err.name === 'StrictPopulateError') {
-    // Return err with response
     return res.status(500).json({
       message: 'Failed to populate provided path',
     });
   }
 
-  // If error is a cast error
+  // Cast error
   if (err.name === 'CastError') {
-    // Path
     const path = err.path;
-
-    // Return err with response
     return res.status(500).json({
       message: `Please provide a valid ${path}`,
     });
   }
 
-  // If error is a validation error
+  // Invalid key
   if (err.name === 'ValidationError') {
-    // key
     const key = err.message.split(':')[1].trim();
-
-    // Return err with response
     return res.status(500).json({
       message: `Please provide a valid ${key}`,
     });
   }
 
-  // If error is a mongoose error
+  // Document not found
   if (err.name === 'DocumentNotFoundError') {
-    // Key
-    const key = Object.keys(err.query)[0];
-
-    // Value
-    const value = Object.values(err.query)[0];
-
-    // Model name
     const model = err.message
       .split(' ')
       [err.message.split(' ').length - 1].replaceAll('"', '');
 
-    // Return err with response
     return res.status(500).json({
-      message: `${model} with ${key} ${value} is not found`,
+      message: `No ${model} found`,
     });
   }
 
-  // If error is a duplicate key error
+  // Duplicate key error
   if (err.name === 'MongoServerError' && err.code === 11000) {
-    // Find the key
     const key = Object.keys(err.keyValue)[0];
-
-    // Return err with response
     return res.status(500).json({
       message: `Please provide a unique ${key}`,
     });
   }
 
-  // If jwt token is expired
+  // Expired JWT
   if (err.name === 'TokenExpiredError') {
-    // Send the error message
-    return res
-      .status(500)
-      .json({ message: 'Token expired, please request the service again' });
+    return res.status(500).json({ message: invalidCredentials });
   }
 
-  // If jwt token is invalid
+  // Invalid JWT
   if (err.name === 'JsonWebTokenError') {
-    // Send the error message
-    return res.status(500).json({ message: 'Please provide a valid token' });
+    return res.status(500).json({ message: invalidCredentials });
   }
 
-  // If password salt is invalid
+  // Invalid salt
   if (err.message.includes('Invalid salt')) {
-    // Send the error message
     return res.status(500).json({ message: 'Please provide a valid salt' });
   }
 
   // Stripe signature verification error
   if (err.message.includes('StripeSignatureVerificationError')) {
-    // Send the error message
     return res
       .status(400)
       .json({ message: 'Stripe signature verification failed' });
   }
 
-  // Stripe invalid checkout session id is provided
+  // Stripe invalid checkout session id
   if (err.message.includes('No such checkout.session')) {
-    // Send the error message
     return res
       .status(400)
       .json({ message: 'Please provide a valid checkout session' });
