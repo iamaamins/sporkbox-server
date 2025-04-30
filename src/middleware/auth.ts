521d564e387d08d9a333a614cd5b9890346b5022
Alamin Shaikh <hello@octib.com>
2025-04-30 09:32:07 +0600

    Added logic to prevent inactive users to login and access resources

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -8,39 +8,38 @@ export default async function handler(
   res: Response,
   next: NextFunction
 ) {
-  if (!req.cookies) {
-    console.log(unAuthorized);
-    res.status(401);
-    throw new Error(unAuthorized);
-  }
-
-  const { token } = req.cookies;
-  if (!token) {
-    console.log(unAuthorized);
+  if (!req.cookies || !req.cookies.token) {
+    console.error(unAuthorized);
     res.status(401);
     throw new Error(unAuthorized);
   }
 
   try {
     const decoded = jwt.verify(
-      token,
+      req.cookies.token,
       process.env.JWT_SECRET as string
     ) as JwtPayload;
 
     const user = await User.findById(decoded._id)
-      .select('-__v -password -updatedAt -createdAt')
+      .select('-__v -password -updatedAt')
       .lean();
 
     if (!user) {
-      console.log(unAuthorized);
+      console.error(unAuthorized);
       res.status(401);
+      throw new Error(unAuthorized);
+    }
+
+    if (user.status !== 'ACTIVE') {
+      console.error(unAuthorized);
+      res.status(403);
       throw new Error(unAuthorized);
     }
 
     req.user = user;
     next();
   } catch (err) {
-    console.log(err);
+    console.error(err);
     throw err;
   }
 }
